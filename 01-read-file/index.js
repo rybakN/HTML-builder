39891c166c53be1f741234134b2b03752c1f3a5a
rybakN <dnb4949@gmail.com>
2022-05-18 12:54:48 +0300

    feat: complete first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
