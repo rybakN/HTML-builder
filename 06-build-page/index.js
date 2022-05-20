e98c14e4e113cb745ed76251ecc3e9eabc225c70
rybakN <dnb4949@gmail.com>
2022-05-20 23:07:52 +0300

    feat: task 6, add function copyFiles

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,67 +2,63 @@ const fs = require('fs');
 const path = require('path');
 
 /* ********************************** Создание папки project-dist **************************** */
-fs.mkdir('./06-build-page/project-dist', { recursive: true }, (err) => {
+
+function mkDir (path) {
+  fs.mkdir(path, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+}
+
+
+/* ********************************** Сборка стилей **************************** */
+function packStyle (readPath, writePath) {
+  fs.readdir(readPath, {withFileTypes: true}, (err, files) => {
+    if (err)
+      console.log(err);
+    else {  
+      let data = '';
+      files.forEach(file => {
+        if (file.isFile()) {
+          let ext = file.name.split('.');
+          if (ext[1] == 'css') {
+            const input = fs.createReadStream(path.resolve(readPath, file.name), 'utf-8');
+            const output = fs.createWriteStream(writePath);
+            input.on('data', chunk => data += chunk);
+            input.on('end', () => output.write(data));
+            
+          }
+        }
+      });}
+  });
+}
+
+function copyFiles (pathDir, originPathFile) {
+
+  fs.mkdir(pathDir, { recursive: true }, (err) => {
     if (err) throw err;
   });
 
-/* ********************************** Сборка стилей **************************** */
-fs.readdir('./06-build-page/styles', {withFileTypes: true}, (err, files) => {
-  if (err)
-    console.log(err);
-  else {  
-    let data = '';
-    files.forEach(file => {
-      if (file.isFile()) {
-        let ext = file.name.split('.');
-        if (ext[1] == 'css') {
-          const input = fs.createReadStream(path.resolve('./06-build-page/styles', file.name), 'utf-8');
-          const output = fs.createWriteStream('./06-build-page/project-dist/style.css');
-          input.on('data', chunk => data += chunk);
-          input.on('end', () => output.write(data));
-          
+  fs.readdir(originPathFile, {withFileTypes: true}, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.copyFile(path.resolve(originPathFile, file.name),path.resolve(pathDir, file.name), (err) => {
+            if (err) throw err;
+          });
         }
-      }
-    });}
-});
+      });}
+  });
+}
 
 
-function copyDir () {
-  
-    fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, (err) => {
-      if (err) throw err;
-    });
-  
-    fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
-      if (err)
-        console.log(err);
-      else {
-        files.forEach(file => {
-          if (file.isFile()) {
-            fs.copyFile(path.resolve('./04-copy-directory/files', file.name),path.resolve('./04-copy-directory/files-copy', file.name), (err) => {
-              if (err) throw err;
-            });
-          }
-        });}
-    });
-    fs.readdir('./04-copy-directory/files-copy', {withFileTypes: true}, (err, files) => {
-      if (err)
-        console.log(err);
-      else {
-        files.forEach(file => {
-          if (file.isFile()) {
-            fs.exists(path.resolve('./04-copy-directory/files', file.name), (err) => {
-              if (!err) {
-                fs.unlink(path.resolve('./04-copy-directory/files-copy', file.name), err => {
-                  if (err) {
-                    console.log(err);
-                  } 
-                });
-              }
-            });
-          }
-        });}
-    });
-  }
-  
-copyDir();
+mkDir ('./06-build-page/project-dist');
+mkDir ('./06-build-page/project-dist/assets');
+mkDir ('./06-build-page/project-dist/assets/fonts');
+mkDir ('./06-build-page/project-dist/assets/img');
+mkDir ('./06-build-page/project-dist/assets/svg');
+packStyle ('./06-build-page/styles', './06-build-page/project-dist/style.css');
+copyFiles ('./06-build-page/project-dist/assets/fonts', './06-build-page/assets/fonts');
+copyFiles ('./06-build-page/project-dist/assets/img', './06-build-page/assets/img');
+copyFiles ('./06-build-page/project-dist/assets/svg', './06-build-page/assets/svg');
